4bf912590452d62555878cef3911d5c7a26dd3b3
charan-1711 <charan.a1711@gmail.com>
2025-03-28 17:46:01 +1100

    test v2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import { FaCopyright } from "react-icons/fa";
 export default function Footer(){
     return(
-        <div className="flex flex-row bg-cyan-200 w-screen p-2 m-2 justify-center space-x-2">
+        <footer className="flex flex-row bg-cyan-200 w-screen p-2 m-2 justify-center space-x-2">
             <h1>Charanraj</h1>
             <FaCopyright className="text-white font-2xl" />
             <p className="">All rights reserved</p>
-        </div>
+        </footer>
     );
 }
